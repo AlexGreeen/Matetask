3e602c12d26eddc7abb05c455bb7424e765496a4
AlexGreeen <krintest1@gmail.com>
2019-03-11 09:48:50 +0200

    Update behaviour when input is wrong

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,75 +2,82 @@ import java.util.Scanner;
 
 public class Calculator {
 
-  public static void main(String[] args) {
-    System.out.println(
-        "Hi there. The Calculator greets you. I \n"
-            + "can compute addition, subtraction, multiplication \n"
-            + "and division with two arabic digit or roman:\n"
-            + "\"1 + 1\" or \"II + V\". You have to use \n"
-            + "single space. Mixing of two types\n"
-            + "in single expression isn't valid: \"1 + IV\" won't \n"
-            + "be computed. Good luck, have fun.");
-    System.out.println("Please enter an expression:");
+    public static void main(String[] args) throws NumberFormatException {
+        System.out.println(
+                "Hi there. The Calculator greets you. I \n"
+                        + "can compute addition, subtraction, multiplication \n"
+                        + "and division with two arabic digit or roman:\n"
+                        + "\"1 + 1\" or \"II + V\". You have to use \n"
+                        + "single space. Mixing of two types\n"
+                        + "in single expression isn't valid: \"1 + IV\" won't \n"
+                        + "be computed. Good luck, have fun.");
+        System.out.println("Please enter an expression:");
 
-    Scanner scanner = new Scanner(System.in);
-    String[] tokens = scanner.nextLine().split(" ");
+        Scanner scanner = new Scanner(System.in);
+        String[] tokens = scanner.nextLine().split(" ");
 
-    int type = arabicOrRoman(tokens);
-    RomanNumeral romanNumeral = new RomanNumeral();
+        int type = arabicOrRoman(tokens);
+        RomanNumeral romanNumeral = new RomanNumeral();
 
-    System.out.println(compute(type, tokens, romanNumeral));
-  }
-
-
-  @org.jetbrains.annotations.NotNull
-  private static String compute(int type, String[] tokens, RomanNumeral rn) {
-    int result = 0;
-    int operand1;
-    int operand2;
-
-    if (type == 0) {
-      operand1 = rn.toArabic(tokens[0]);
-      operand2 = rn.toArabic(tokens[2]);
-    } else {
-      operand1 = Integer.valueOf(tokens[0]);
-      operand2 = Integer.valueOf(tokens[2]);
+        System.out.println(compute(type, tokens, romanNumeral));
     }
 
-    switch (tokens[1].charAt(0)) {
-      case '+':
-        result = operand1 + operand2;
-        break;
-      case '-':
-        result = operand1 - operand2;
-        break;
-      case '*':
-        result = operand1 * operand2;
-        break;
-      case '/':
-        if (operand1 == 0 || operand2 == 0) {
-          throw new IllegalArgumentException("Argument divisor is 0");
+
+    @org.jetbrains.annotations.NotNull
+    private static String compute(int type, String[] tokens, RomanNumeral rn) {
+        int result = 0;
+        int operand1;
+        int operand2;
+
+        if (type == -1) {
+            System.out.println("Invalid input. Good bye.");
+            System.exit(0);
         }
-        result = operand1 / operand2;
-        break;
-    }
-    if (type == 0) return rn.toRoman(result);
 
-    return String.valueOf(result);
-  }
+        if (type == 0) {
+            operand1 = rn.toArabic(tokens[0]);
+            operand2 = rn.toArabic(tokens[2]);
+        } else {
+            operand1 = Integer.valueOf(tokens[0]);
+            operand2 = Integer.valueOf(tokens[2]);
+        }
 
-  /* Check which numerals used and return
-   1 if Arabic
-   0 if Roman
-   and -1 if input is incorrect*/
-  private static int arabicOrRoman(String[] tokens) {
-    if (InputValidator.isArabic(tokens)) {
-      return 1;
-    }
-    if (InputValidator.isRoman(tokens)) {
-      return 0;
+
+
+        switch (tokens[1].charAt(0)) {
+            case '+':
+                result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*':
+                result = operand1 * operand2;
+                break;
+            case '/':
+                if (operand1 == 0 || operand2 == 0) {
+                    throw new IllegalArgumentException("Argument divisor is 0");
+                }
+                result = operand1 / operand2;
+                break;
+        }
+        if (type == 0) return rn.toRoman(result);
+
+        return String.valueOf(result);
     }
 
-    return -1;
-  }
+    /* Check which numerals used and return
+     1 if Arabic
+     0 if Roman
+     and -1 if input is incorrect*/
+    private static int arabicOrRoman(String[] tokens) {
+        if (InputValidator.isArabic(tokens)) {
+            return 1;
+        }
+        if (InputValidator.isRoman(tokens)) {
+            return 0;
+        }
+
+        return -1;
+    }
 }
--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -10,7 +10,7 @@ class InputValidator {
         Pattern pattern =
         Pattern.compile("^(?i:(?=[MDCLXVI])((M{0,3})((C[DM])|(D?C{0,3}))?((X[LC])|(L?XX{0,2})|L)?((I[VX])|(V?(II{0,2}))|V)?))$");
         return pattern.matcher(token1).matches() && pattern.matcher(token2).matches();
-    }
+     }
 
     // determines whether expression uses arabic numerals only
      static boolean isArabic(String[] s) {
@@ -18,5 +18,7 @@ class InputValidator {
         String token2 = s[2];
         Pattern pattern = Pattern.compile("^[\\u0621-\\u064A0-9 ]+$");
         return pattern.matcher(token1).matches() && pattern.matcher(token2).matches();
-    }
+     }
+
+
 }
